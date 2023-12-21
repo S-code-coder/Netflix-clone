33b30cebcefe95f025d5fc5fa8cd63c7820dc8ba
S-code-coder <farinmadesamuel5@gmail.com>
2023-12-21 01:03:54 +0100

    Added Netflix-clone to repositories

---
+++ src/request.js
@@ -0,0 +1,14 @@
+const API_KEY = '3f7545c0e4d355aa517c4a94bca0bac5';
+
+const requests = {
+    fetchTrending:`/trending/all/week?api_key=${API_KEY}&language=en-us`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_network=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${API_KEY}&language-US`,
+    fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
